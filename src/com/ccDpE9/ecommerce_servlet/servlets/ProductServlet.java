bfccc805f14dd272b6200bd647301480b438f027
ccDpE9 <z_eon00@protonmail.com>
2020-04-07 22:55:25 +0200

    Product Servlet/Bean

---
+++ src/com/ccDpE9/ecommerce_servlet/servlets/ProductServlet.java
@@ -0,0 +1,65 @@
+package com.ccDpE9.ecommerce_servlet.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ccDpE9.ecommerce_servlet.beans.Product;
+import com.ccDpE9.ecommerce_servlet.dao.ApplicationDao;
+
+/**
+ * Servlet implementation class ProductServlet
+ */
+@WebServlet("/ProductServlet")
+public class ProductServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProductServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    response.setContentType("text/html");
+	    PrintWriter out = response.getWriter();
+
+		
+		ApplicationDao dao = new ApplicationDao();
+		List<Product> products = dao.listProducts();
+		
+	    out.println("<html>");
+	    out.println("<head>");
+	    out.println("<title>Hola</title>");
+	    out.println("</head>");
+	    out.println("<body>");
+	    
+	    for (Product p: products) {
+	    	out.println("<p>" + p.getName() + "</p>");
+	    }
+	    
+	    out.println("</body>");
+	    out.println("</html>");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
