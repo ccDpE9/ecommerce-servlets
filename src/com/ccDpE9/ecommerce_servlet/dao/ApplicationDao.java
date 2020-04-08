aaed5f1e547dfa67b596446791fad504e06b5387
ccDpE9 <z_eon00@protonmail.com>
2020-04-08 23:54:07 +0200

    Moved db reference variable to the default constructor, since i'll be using it for basicaly all methods.

--- src/com/ccDpE9/ecommerce_servlet/dao/ApplicationDao.java
+++ src/com/ccDpE9/ecommerce_servlet/dao/ApplicationDao.java
@@ -7,16 +7,22 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.github.ccDpE9.servlets_ecommerce.beans.Product;
-import com.github.ccDpE9.servlets_ecommerce.dao.DBConnection;
+import com.ccDpE9.ecommerce_servlet.beans.Product;
+import com.ccDpE9.ecommerce_servlet.beans.User;
 
 public class ApplicationDao {
+	private Connection connection;
+	
+	public ApplicationDao() {
+		super();
+		this.connection = DBConnection.get();
+	}
+	
 	public List<Product> listProducts() {
 		Product product = null;
 		List<Product> products = new ArrayList<>();
 		
 		try {
-			Connection connection = DBConnection.get();
 			String sql = "select * from products";
 			Statement statement = connection.createStatement();
 			ResultSet set = statement.executeQuery(sql);
@@ -35,5 +41,22 @@ public class ApplicationDao {
 		}
 		
 		return products;
+	}
+	
+	public int registerUser(User user) {
+		int rowsAffected = 0;
+		
+		try {
+			String query = "INSERT INTO users(name, email, password) VALUES ('"+user.getName()+"','"+user.getEmail()+"','"+user.getPassword()+"')";
+			
+			java.sql.PreparedStatement statement = connection.prepareStatement(query);
+			
+			rowsAffected = statement.executeUpdate();
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	
+		return rowsAffected;
 	}
 }
