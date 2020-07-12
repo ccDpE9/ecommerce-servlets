aa335f1fff874b44c37adc9261bb1bea32caad80
badkidspv95hools <ccDpE9@protonmail.com>
2020-07-12 18:22:39 +0200

    Added some attributes and corresponding getters and setters.

--- src/com/ccDpE9/ecommerce_servlet/beans/Product.java
+++ src/com/ccDpE9/ecommerce_servlet/beans/Product.java
@@ -2,16 +2,16 @@ package com.ccDpE9.ecommerce_servlet.beans;
 
 public class Product {
 	private String name;
+	private String description;
+	private double price;
 	private int quantity;
 	private String img;
 	
-	public Product() {
-		super();
-	}
-	
-	public Product(String name, int quantity, String img) {
+	public Product(String name, String description, double price, int quantity, String img) {
 		super();
 		this.name = name;
+		this.description = description;
+		this.price = price;
 		this.quantity = quantity;
 		this.img = img;
 	}
@@ -19,25 +19,40 @@ public class Product {
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getDescription() {
+		return description;
+	}
 	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
 	public int getQuantity() {
 		return quantity;
 	}
-	
+
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
-	
+
 	public String getImg() {
 		return img;
 	}
-	
+
 	public void setImg(String img) {
 		this.img = img;
 	}
-
 }
